51418490304d3a3b3a9f367b168bd0b4d454bcbb
120356aa <yurlovandrew@gmail.com>
2019-04-10 19:22:03 -0700

    succeed test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,8 +1,17 @@
 const { secceed, fail, repair, get } = require('./enhancer.js');
 
 describe('enhancer', () => {
-  const result = repair({ durability: 0 });
+  const result = repair({ name: 'long sword', durability: 0, enchantment: 0 })
   it('tests repair', () => {
     expect(result.durability).toBe(100);
-  })
+  });
+
+  it('succeed, enchantment tester', () => {
+    const succeedResult = succeed({
+      name: 'long sword',
+      durability: 100,
+      enchantment: 0
+    })
+    expect(succeedResult.enchantment).toBe(1)
+  });
 })
